f39e4f161e94e5adb00174750e1f2beb09aea10f
Juan Reyes <camiloreyes1125@gmail.com>
2025-03-23 16:35:57 -0500

    endpoints tested

--- obj/Debug/net9.0/MyNewApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyNewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyNewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa6c246d3eaf1f9223a00a13ceb4290e88e8db79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59aba3a0917f2a800d306d00ca4f5bc851a763d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyNewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyNewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
